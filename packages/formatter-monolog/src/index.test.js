633ad0ed7b7dd9e6c3d0432e696b039dcf9d892f
Steven Weingärtner <sweingaertner@brainbits.net>
2019-04-02 17:32:24 +0200

    TSPM-728 Fix formatter tests

--- packages/formatter-monolog/src/index.test.js
+++ packages/formatter-monolog/src/index.test.js
@@ -4,6 +4,15 @@ import monolog from './index';
 jest.mock('date-fns');
 format.mockReturnValue('2010-01-31 23:59:59');
 
+function createEvent(channel, level, message, meta) {
+    return {
+        channel,
+        level,
+        message,
+        meta,
+    };
+}
+
 describe('monolog function', () => {
     let error;
     beforeEach(() => {
@@ -13,18 +22,19 @@ describe('monolog function', () => {
             at Object.foo (/src/foo.js:18:28)
         `;
     });
+
     it('should get the correct channel name', () => {
-        expect(monolog('tests', 'level', { message: 'message' }))
+        expect(monolog(createEvent('tests', 'level', { message: 'message' })))
             .toEqual('[2010-01-31 23:59:59] tests.LEVEL: message [] []');
     });
 
     it('should return a log formatted message', () => {
-        expect(monolog('tests', 'debug', { message: 'my message' }))
+        expect(monolog(createEvent('tests', 'debug', { message: 'my message' })))
             .toEqual('[2010-01-31 23:59:59] tests.DEBUG: my message [] []');
     });
 
     it('should handle a string in property message', () => {
-        expect(monolog('tests', 'info', { message: 'this should be a string' }))
+        expect(monolog(createEvent('tests', 'info', { message: 'this should be a string' })))
             .toEqual('[2010-01-31 23:59:59] tests.INFO: this should be a string [] []');
     });
 
@@ -35,47 +45,48 @@ describe('monolog function', () => {
             foo: '!!!!',
             message: 'foo bar lib message',
         };
-        expect(monolog('tests', 'info', message))
+        expect(monolog(createEvent('tests', 'info', message)))
             .toEqual('[2010-01-31 23:59:59] tests.INFO: foo bar lib message [] {"type":"message","content":"this is my content","foo":"!!!!"}');
     });
 
     it('should handle an error', () => {
-        expect(monolog('tests', 'error', error))
+        expect(monolog(createEvent('tests', 'error', error)))
             .toEqual('[2010-01-31 23:59:59] tests.ERROR: Error - This is a fake error {"stackTrace":["at Object.blah (/src/blah.js:18:28)","at Object.foo (/src/foo.js:18:28)"]} []');
     });
 
     it('should handle unknown error types', () => {
         error.locations = [{ line: 16, column: 5 }];
-        expect(monolog('tests', 'error', error))
+        expect(monolog(createEvent('tests', 'error', error)))
             .toEqual('[2010-01-31 23:59:59] tests.ERROR: Error - This is a fake error {"stackTrace":["at Object.blah (/src/blah.js:18:28)","at Object.foo (/src/foo.js:18:28)"]} {"locations":[{"line":16,"column":5}]}');
     });
 
     it('should handle unknown error types with metadata', () => {
         const meta = { file: 'src/monolog.js:16:5' };
-        expect(monolog('tests', 'error', error, meta))
+        expect(monolog(createEvent('tests', 'error', error, meta)))
             .toEqual('[2010-01-31 23:59:59] tests.ERROR: Error - This is a fake error {"stackTrace":["at Object.blah (/src/blah.js:18:28)","at Object.foo (/src/foo.js:18:28)"],"file":"src/monolog.js:16:5"} []');
     });
 
     it('should handle other variable types', () => {
         const info = ['my message'];
 
-        expect(monolog('tests', 'debug', info))
+        expect(monolog(createEvent('tests', 'debug', info)))
             .toEqual('[2010-01-31 23:59:59] tests.DEBUG: my message [] []');
     });
+
     describe('Stacktrace handling', () => {
         it('should not add a stacktrace if it is undefined', () => {
             error.stack = undefined;
-            expect(monolog('tests', 'error', error))
+            expect(monolog(createEvent('tests', 'error', error)))
                 .not.toContain('{"stackTrace":["at ');
         });
 
         it('should not add the error itself to the stacktrace', () => {
-            expect(monolog('tests', 'error', error))
+            expect(monolog(createEvent('tests', 'error', error)))
                 .not.toContain('"Error - This is a fake error"');
         });
 
         it('should handle a stacktrace in error types', () => {
-            expect(monolog('tests', 'error', error))
+            expect(monolog(createEvent('tests', 'error', error)))
                 .toEqual('[2010-01-31 23:59:59] tests.ERROR: Error - This is a fake error {"stackTrace":["at Object.blah (/src/blah.js:18:28)","at Object.foo (/src/foo.js:18:28)"]} []');
         });
 
@@ -97,8 +108,8 @@ describe('monolog function', () => {
                 at Object.should.not.be.logged (/src/should.not.be.logged.js:18:28)
                 at Object.should.not.be.logged (/src/should.not.be.logged.js:18:28)
                 at Object.should.not.be.logged (/src/should.not.be.logged.js:18:28)`;
-            expect(monolog('tests', 'error', error)).not.toContain('at Object.should.not.be.logged');
-            expect(monolog('tests', 'error', error)).toContain('5 more lines ...');
+            expect(monolog(createEvent('tests', 'error', error))).not.toContain('at Object.should.not.be.logged');
+            expect(monolog(createEvent('tests', 'error', error))).toContain('5 more lines ...');
         });
 
         it('should use singular for one truncated line', () => {
@@ -115,8 +126,8 @@ describe('monolog function', () => {
                 at Object.guzzle (/src/guzzle.js:18:28)
                 at Object.gizzle (/src/gizzle.js:18:28)
                 at Object.should.not.be.logged (/src/should.not.be.logged.js:18:28)`;
-            expect(monolog('tests', 'error', error)).not.toContain('at Object.should.not.be.logged');
-            expect(monolog('tests', 'error', error)).toContain('1 more line ...');
+            expect(monolog(createEvent('tests', 'error', error))).not.toContain('at Object.should.not.be.logged');
+            expect(monolog(createEvent('tests', 'error', error))).toContain('1 more line ...');
         });
 
         it('should not use truncated line feature if it is shorter than 10 lines', () => {
@@ -132,7 +143,7 @@ describe('monolog function', () => {
                 at Object.frizzle (/src/frizzle.js:18:28)
                 at Object.guzzle (/src/guzzle.js:18:28)
                 at Object.gizzle (/src/gizzle.js:18:28)`;
-            expect(monolog('tests', 'error', error)).not.toContain('...');
+            expect(monolog(createEvent('tests', 'error', error))).not.toContain('...');
         });
     });
 });
